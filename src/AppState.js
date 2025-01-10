507780606a0da7253dd18a68398c1db5e759b6dd
ThomasWSnider <thomas.wayne.snider@gmail.com>
2025-01-10 14:02:19 -0700

    fixed image rendering issue

--- src/AppState.js
+++ src/AppState.js
@@ -1,4 +1,8 @@
 import { reactive } from 'vue'
+// @ts-ignore
+import keeperScreenshot from '../public/img/KeeprScreenshot.png'
+// @ts-ignore
+import MindPaletteScreenshot from '../public/img/MindPaletteScreenshot.png'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
@@ -56,13 +60,13 @@ export const AppState = reactive({
   projects: [
     {
       title: 'Keepr',
-      img: '/img/KeeprScreenshot.png',
+      img: keeperScreenshot,
       description: 'Keepr is a social media app that allows users to explore, create, and organize keeps into personal vaults, with options for private or public vaults. Built with Vue.js and Vue Router for the frontend, a .NET Web API backend using the Repository pattern, and MySQL with Dapper for the database, the app integrates Auth0 for secure authentication. Key features include engagement tracking for keeps, detailed views, and private vault functionality. The project follows a Figma design mock-up, adhering to UI/UX best practices, and meets all functionality through a comprehensive API test suite.',
       link: 'https://keepr.twsnider.work/'
     },
     {
       title: 'Mind Palette',
-      img: '/img/MindPaletteScreenshot.png',
+      img: MindPaletteScreenshot,
       description: 'Mind Palette is a flashcard study app designed to help users efficiently create, manage, and study flashcards. Built with Vue.js, Node.js, Express, and MongoDB, the app features user registration and secure authentication via Auth0. Users can organize flashcards into decks, and engage with the study mode, which mimics the experience of physical flashcards. The app is responsive, user-friendly, and optimized for both desktop and mobile. MindPalette offers a seamless learning experience through its intuitive interface and robust functionality.',
       link: 'https://mindpalette.twsnider.work/'
     },
